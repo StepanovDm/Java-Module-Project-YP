76c30d08e5c7dc425b7a173c57fd0b74be449e2a
дмитрий с <stepanov_dmitriy_ufa@mail.ru>
2024-02-22 20:18:33 +0300

    StepanovDm

---
+++ src/main/java/org/example/NormFormat.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class NormFormat {
+    public static String returnRub(double sum) {
+        int value = (int) sum % 100;
+        String rub = "";
+        if (value >= 10 & value <= 20) {
+
+            rub = "Рублей";
+
+        } else {
+            switch (value % 10) {
+                case 1:
+                    rub = "Рубль";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    rub = "Рубля";
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 0:
+                    rub = "Рублей";
+            }
+        }
+        return String.format("%.2f",sum) + " " + rub;
+    }
+}

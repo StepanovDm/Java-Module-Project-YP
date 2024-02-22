9abe4cc781c9bc74255f42b95722887b0a8aa671
дмитрий с <stepanov_dmitriy_ufa@mail.ru>
2024-02-22 22:46:37 +0300

    stepanovDming

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import static org.example.NormFormat.returnRub;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
